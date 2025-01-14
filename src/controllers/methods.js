542956ecc5094dd3844696601b10dc43acea2a62
mendesftrader <mendesftrader@gmail.com>
2025-01-14 14:37:33 -0400

    adicionando metodo get

--- src/controllers/methods.js
+++ src/controllers/methods.js
@@ -1,7 +1,12 @@
 const ProductsModel = require('../models/products')
 
+// função para localizar os items pelo ID
 async function get(req, res){
-    const products = await ProductsModel.find()
+    const {id} = req.params
+
+    const obj = id ? { _id: id} : null
+
+    const products = await ProductsModel.find(obj)
 
     res.send(products)
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const ProductsController = require('../controllers/methods')
 
-router.get('/products', ProductsController.get)
+router.get('/products/:id?', ProductsController.get)
 //router.post('/products', ProductsController.post)
 //router.put('/products', ProductsController.put)
 //router.delete('/products', ProductsController.delete)
